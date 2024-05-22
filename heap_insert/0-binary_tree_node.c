2fd782dbad0d7ca8db4f49df6ddaf8cbc8be2f6a
SpStigma <githubstigma@gmail.com>
2024-05-22 15:57:56 +0200

    ADD: Task 0.

---
+++ heap_insert/0-binary_tree_node.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+* binary_tree_node - create a binary tree.
+* @parent: pointer to the parent node.
+* @n: value of the node.
+* Return: A pointer to the node or return NULL.
+**/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
+
+    if (newNode == NULL)
+    {
+        return NULL;
+    }
+
+    newNode->parent = parent;
+    newNode->n = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    return newNode;
+}

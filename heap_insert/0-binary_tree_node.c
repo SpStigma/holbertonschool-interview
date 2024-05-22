ad3a6904592f1d0d1f6a74b544daee63fcb54bfd
SpStigma <githubstigma@gmail.com>
2024-05-22 16:20:27 +0200

    Format document betty style.

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -2,23 +2,23 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - creates a binary tree node.
- * @parent: pointer to the parent node.
- * @value: value to put in the new node.
+ * binary_tree_node - Creates a binary tree node.
+ * @parent: Pointer to the parent node.
+ * @value: Value of the node.
  *
- * Return: pointer to the new node, or NULL on failure.
+ * Return: A pointer to the newly created node, or NULL on failure.
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
+	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
 
-    if (newNode == NULL)
-        return NULL;
+	if (newNode == NULL)
+		return (NULL);
 
-    newNode->parent = parent;
-    newNode->n = value;
-    newNode->left = NULL;
-    newNode->right = NULL;
+	newNode->parent = parent;
+	newNode->n = value;
+	newNode->left = NULL;
+	newNode->right = NULL;
 
-    return (newNode);
+	return (newNode);
 }

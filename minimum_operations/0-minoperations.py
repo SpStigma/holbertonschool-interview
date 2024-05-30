f92515141c11f1391c9eefa064b4483617a9d8db
SpStigma <githubstigma@gmail.com>
2024-05-30 16:21:11 +0200

    PEP8 validator.

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """Minimum Operations"""
 
+
 def minOperations(n):
-    """Return the minimu operation with nombre premier."""
+    """Return the minimum operation with prime number."""
     if n <= 1:
         return 0
-    
+
     operations = 0
     divisor = 2
-    
+
     while n > 1:
         while n % divisor == 0:
             operations += divisor
             n //= divisor
         divisor += 1
-    
+
     return operations

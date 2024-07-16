1a20cd7de0ec78d6f24338d75b8154e2ed688686
SpStigma <githubstigma@gmail.com>
2024-07-16 12:07:19 +0200

    2048 line way of Pierre.

---
+++ slide_line/0-slide_line.c
@@ -0,0 +1,51 @@
+#include "slide_line.h"
+
+/**
+ * slide_line - slides and merges an array of integers, like the game 2048
+ * @line: an array of integers containing size elements
+ * @size: the size of the sliding array
+ * @direction: the direction for sliding & merging (the increment)
+ *
+ * Return: 1 upon success, 0 upon failure
+ */
+
+int slide_line(int *line, size_t size, int direction)
+{
+	int start, end, i, j;
+
+	if (direction == SLIDE_RIGHT)
+	{
+		start = size - 1;
+		end = 0;
+	}
+	else if (direction == SLIDE_LEFT)
+	{
+		start = 0;
+		end = size - 1;
+	}
+	else
+	{
+		return (0);
+	}
+	for (i = start; i != end; i += direction)
+	{
+		for (j = i + direction; j != end + direction; j += direction)
+		{
+			if (line[i] == line[j] && line[i] != 0)
+			/*I don't like it, but the alternatives aren't great either*/
+			{
+				line[i] += line[j];
+				line[j] = 0;
+				break;
+			}
+			if (line[i] == 0)
+			{
+				line[i] += line[j];
+				line[j] = 0;
+			}
+			else if (line[i] != line[j] && line[j] != 0)
+				break;
+		}
+	}
+	return (1);
+}
---
+++ slide_line/slide_line.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#define SLIDE_LEFT 1
+#define SLIDE_RIGHT -1
+#include <stdio.h>
+
+int slide_line(int *line, size_t size, int direction);
+
+#endif

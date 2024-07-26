774a43f8603f10e26cd7e9b91daf80854b4ac718
SpStigma <githubstigma@gmail.com>
2024-07-26 10:52:15 +0200

    Fix: betty style.

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -3,23 +3,24 @@
 #include "menger.h"
 
 /**
- * draw_menger - Draws the character at the given position (x, y) for the sponge.
+ * draw_menger - Draws the character at the given position
+ * (x, y) for the sponge.
  * @x: The x coordinate.
  * @y: The y coordinate.
  * @size: The current size of the grid being processed.
  */
 void draw_menger(int x, int y, int size)
 {
-    while (size > 0)
-    {
-        if ((x / size) % 3 == 1 && (y / size) % 3 == 1)
-        {
-            printf(" ");
-            return;
-        }
-        size /= 3;
-    }
-    printf("#");
+	while (size > 0)
+	{
+		if ((x / size) % 3 == 1 && (y / size) % 3 == 1)
+		{
+			printf(" ");
+			return;
+		}
+		size /= 3;
+	}
+	printf("#");
 }
 
 /**
@@ -28,21 +29,21 @@ void draw_menger(int x, int y, int size)
  */
 void menger(int level)
 {
-    int size;
-    int x, y;
+	int size;
+	int x, y;
 
-    if (level < 0)
-    {
-        return;
-    }
+	if (level < 0)
+	{
+		return;
+	}
 
-    size = (int)pow(3, level);
-    for (y = 0; y < size; y++)
-    {
-        for (x = 0; x < size; x++)
-        {
-            draw_menger(x, y, size);
-        }
-        printf("\n");
-    }
+	size = (int)pow(3, level);
+	for (y = 0; y < size; y++)
+	{
+		for (x = 0; x < size; x++)
+		{
+			draw_menger(x, y, size);
+		}
+		printf("\n");
+	}
 }

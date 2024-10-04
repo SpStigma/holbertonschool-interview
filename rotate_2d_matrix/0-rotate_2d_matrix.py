269e447e242b250897e2d51bcb82bc32eefb5500
SpStigma <githubstigma@gmail.com>
2024-10-04 09:20:32 +0200

    Fix: PEP8.

--- rotate_2d_matrix/0-rotate_2d_matrix.py
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,12 +4,10 @@
 
 def rotate_2d_matrix(matrix):
     """Fonction to rotate the module"""
-    #Transpose the matrix by swapping rows and columns
+    # Transpose the matrix by swapping rows and columns
     n = len(matrix)
     for i in range(n):
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    
-
     for i in range(n):
         matrix[i].reverse()

6d13e78b94d5fcd3df6d957a5b717c9a59b03202
SpStigma <githubstigma@gmail.com>
2024-11-28 19:00:17 +0100

    Fix: betty style.

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -1,12 +1,15 @@
 #include "holberton.h"
 
 /**
- * wildcmp - Compares two strings, considering '*' as a special character
- *           that can match any string, including an empty string.
+ * wildcmp - Compares two strings, considering '*'
+ * as a special character
+ * that can match any string, including an empty string.
  * @s1: The first string to compare.
- * @s2: The second string, which may contain the special character '*'.
+ * @s2: The second string, which may contain the special
+ * character '*'.
  *
- * Return: 1 if the strings can be considered identical, otherwise 0.
+ * Return: 1 if the strings can be considered identical, 
+ * otherwise 0.
  */
 int wildcmp(char *s1, char *s2)
 {

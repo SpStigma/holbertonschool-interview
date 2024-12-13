03d6f4e334335c6015059ca3423b188b7b30f98d
SpStigma <githubstigma@gmail.com>
2024-12-13 09:37:39 +0100

    ADD: function that checks whether a given pattern matches a given string.

---
+++ regex/regex.c
@@ -0,0 +1,34 @@
+#include "regex.h"
+
+/**
+ * regex_match - Checks if a pattern matches a given string.
+ * @str: The string to scan.
+ * @pattern: The regular expression pattern.
+ *
+ * Return: 1 if the pattern matches the string, otherwise 0.
+ */
+int regex_match(const char *str, const char *pattern)
+{
+	int first_match;
+
+	/* If the pattern is empty, the string must also be empty */
+	if (*pattern == '\0')
+		return (*str == '\0');
+
+	/* Check if the first character matches */
+	first_match = (*str != '\0' && (*str == *pattern || *pattern == '.'));
+
+	/* Handle '*' in the pattern */
+	if (*(pattern + 1) == '*')
+	{
+		/* Two options:
+		 * 1. Ignore the 'x*' in the pattern.
+		 * 2. Consume one character if the first matches.
+		 */
+		return (regex_match(str, pattern + 2) ||
+			(first_match && regex_match(str + 1, pattern)));
+	}
+
+	/* Proceed with the next characters */
+	return (first_match && regex_match(str + 1, pattern + 1));
+}
